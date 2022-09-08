e3e7e1cc583ec97e97cfb864a8b45a0afe596613
RobertTMZW <mukuchurart@gmail.com>
2022-09-07 23:52:06 -0500

    Hiding exposed key

--- smartnotes/settings.py
+++ smartnotes/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&6qg0r0za)3()@*@hkp)y#%g^kk8rj&@)zr5+z5f8x9@r=_b&o'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
